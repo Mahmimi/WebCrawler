d055a170cd35535d4b2a0cdd49fcba9589407650
Mahmimi <Jiranuwat.contact@gmail.com>
2024-07-14 23:09:53 +0700

    update chrome option

--- WebScraping_Crawler.py
+++ WebScraping_Crawler.py
@@ -54,6 +54,11 @@ class SinglePage_WebCrawler():
             options = webdriver.ChromeOptions()
             options.add_argument('--headless')
             options.add_argument('--disable-gpu')
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--window-size=1920,1080")  # Set window size
+            options.add_argument("start-maximized")  # Maximize window
+            options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")  # Set User-Agent
             driver = webdriver.Chrome(options=options)
             driver.get(self.url)
             driver.implicitly_wait(3)
